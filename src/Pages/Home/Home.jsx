c55f52e1b259e4688e59f8ba46f94de2438aeb77
Arshpreet <arshpreet27014@gmail.com>
2024-10-20 22:44:27 +0530

    initial

---
+++ src/Pages/Home/Home.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import './Home.css'
+import Header from '../../Components/Header/Header'
+import ExploreMenu from '../../Components/ExploreMenu/ExploreMenu'
+import { useState } from 'react'
+import FoodDisplay from '../../FoodDisplay/FoodDisplay'
+import AppDownload from '../../Components/AppDownload/AppDownload'
+
+const Home = () => {
+
+  const [category,setCategory]=useState("All");
+  return (
+    <div>
+      <Header/>
+      <ExploreMenu category={category} setCategory={setCategory}/>
+      <FoodDisplay category={category}/>
+      <AppDownload/>
+
+    </div>
+  )
+}
+
+export default Home
